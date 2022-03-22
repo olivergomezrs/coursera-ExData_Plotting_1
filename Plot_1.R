7dca1b9a8c146bf70ed209ac314b33cf62f84de7
Oliver Gómez <90093706+olivergomezrs@users.noreply.github.com>
2022-03-21 22:52:18 -0600

    Update Plot_1.R

--- Plot_1.R
+++ Plot_1.R
@@ -3,7 +3,7 @@ download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
 unzip ("data.zip", exdir = "./")
 
 #read data
-datapower <- read.table("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";")
+datapower <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
 data <- datapower[(datapower$Date=="1/2/2007" | datapower$Date=="2/2/2007" ), ]
 
 ## Plot 1
